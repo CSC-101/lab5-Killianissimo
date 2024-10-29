bd87699ef759a4f2fd71fe6750b9949aae8c4014
killian <kmurph41@calpoly.edu>
2024-10-28 23:26:37 -0700

    yay

--- lab5.py
+++ lab5.py
@@ -1,4 +1,5 @@
 import data
+import math
 
 # Write your functions for each part in the space below.
 
@@ -11,12 +12,64 @@ import data
 
 
 # Part 3
-
+'''This function takes 2 Times and adds them together, being careful to not create a Time with 
+a minute or second value over 59. It inputs 2 Times and outputs 1 Time. 
+'''
+def time_add(time1:data.Time, time2:data.Time) -> data.Time:
+    newTime = data.Time(0,0,0)
+    newTime.hour = time1.hour + time2.hour
+    newTime.minute = time1.minute + time2.minute
+    newTime.second = time1.second + time2.second
+    if newTime.second > 59:
+        newTime.minute += 1
+        newTime.second -= 60
+    if newTime.minute > 59:
+        newTime.hour += 1
+        newTime.minute -= 60
+    return newTime
 
 # Part 4
-
+'''This function takes a list of floats and tests if it sorted in descending order. If it is, 
+it returns the bool True. If not, it returns nothing. 
+'''
+def is_descending(values:list[float]) -> bool:
+    check = 0
+    for i in range(len(values) - 1):
+        if values[i] < values[i + 1]:
+            check += 1
+    if check == 0:
+        return True
 
 # Part 5
-
+'''This function inputs a list and two integers and outputs either None or an int. This function
+takes the input list and treating the integers and indexes, finds the largest value in the list
+between the two indexes and returns it. However, if the first index is larger than the second, 
+the function returns None. 
+'''
+def largest_between(vals:list[int], lower:int, upper:int) -> None or int:
+    if lower > upper:
+        return None
+    biggest = lower
+    for i in range(lower, upper + 1):
+        if vals[i] > biggest:
+            biggest = vals[i]
+    return biggest
 
 # Part 6
+'''This function inputs a list of Points and outputs an integer. The function takes the input list
+and finds the distance from each Point to the origin. It then returns the index of the Point in
+the list furthest from the origin.
+'''
+def furthest_from_origin(pts:list[data.Point])->None or data.Point:
+    furthest_distance = 0
+    furthest_index = 0
+    if pts == []:
+        return None
+    for i in range(len(pts)):
+        i_distance = math.sqrt((pts[i].x)**2 + (pts[i].y)**2)
+        if i_distance > furthest_distance:
+            furthest_distance = i_distance
+            furthest_index = i
+
+    return furthest_index
+
--- lab5_tests.py
+++ lab5_tests.py
@@ -3,26 +3,62 @@ import lab5
 import unittest
 
 
+
+
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
-    # Part 1
-    #    Part 1 tests should be in data_tests.py.
-
-    # Part 2
-    #    Part 2 tests should be in data_tests.py.
-
-
     # Part 3
-
+    def test_time_add_1(self):
+        time1 = data.Time(1,2,3)
+        time2 = data.Time(1,2,3)
+        testResult = lab5.time_add(time1, time2)
+        self.assertEqual(testResult, data.Time(2,4,6))
+    def test_time_add_2(self):
+        time1 = data.Time(1,50,50)
+        time2 = data.Time(1,40,30)
+        testResult = lab5.time_add(time1, time2)
+        self.assertEqual(testResult, data.Time(3,31,20))
 
     # Part 4
-
+    def test_is_descending_1(self):
+        input = [5,3,2]
+        expected = True
+        testResult = lab5.is_descending(input)
+        self.assertEqual(expected, testResult)
+    def test_is_descending_2(self):
+        input = [5,3,6]
+        expected = None
+        testResult = lab5.is_descending(input)
+        self.assertEqual(expected, testResult)
 
     # Part 5
-
+    def test_largest_between_1(self):
+        input1 = [1,2,3,4,5,6]
+        input2 = 2
+        input3 = 5
+        expected = 6
+        testResult = lab5.largest_between(input1, input2, input3)
+        self.assertEqual(expected, testResult)
+    def test_largest_between_2(self):
+        input1 = [3,6,1,16,1,23]
+        input2 = 2
+        input3 = 5
+        expected = 16
+        testResult = lab5.largest_between(input1, input2, input3)
+        self.assertNotEqual(expected, testResult)
 
     # Part 6
+    def test_furthest_from_origin_1(self):
+        input = [data.Point(1,2),data.Point(3,-4)]
+        expected = 1
+        testResult = lab5.furthest_from_origin(input)
+        self.assertEqual(expected, testResult)
+    def test_furthest_from_origin_2(self):
+        input = []
+        expected = None
+        testResult = lab5.furthest_from_origin(input)
+        self.assertEqual(expected, testResult)
 
 
 
